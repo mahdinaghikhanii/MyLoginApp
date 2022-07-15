6ad31f8c1fb3c1881884d73e0b808e98f5476cee
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-07-15 20:50:12 +0430

    finishid UI and code in signup

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -2,26 +2,30 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.w3c.dom.Text;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intentSignUp = new Intent(this, SignupActivity.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intentSignUp = new Intent(this, SignupActivity.class);
+
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView)  findViewById(R.id.password);
+        TextView signup_text = (TextView) findViewById(R.id.signup_now);
         MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
 
 
@@ -33,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     Toast.makeText(MainActivity.this, "Username or password is not True", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        signup_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentSignUp);
             }
         });
 
--- app/src/main/java/com/example/myloginapp/SignupActivity.java
+++ app/src/main/java/com/example/myloginapp/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Intent intentMainActivity = new Intent(this, MainActivity.class);
+        TextView sign_in_now_text = (TextView) findViewById( R.id.sign_in_now_text);
+        sign_in_now_text.setOnClickListener(view -> startActivity(intentMainActivity));
     }
 }
