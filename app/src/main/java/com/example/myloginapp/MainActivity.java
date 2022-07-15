04a6a785cb445ec509c98174f03a1897408159f7
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-07-15 20:00:02 +0430

    finishid ui and set in new screan

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Intent intentSignUp = new Intent(this, SignupActivity.class);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView)  findViewById(R.id.password);
+        MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
+
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("mahdi") && password.getText().toString().equals("welcome") ){
+                    startActivity(intentSignUp);
+                }else{
+                    Toast.makeText(MainActivity.this, "Username or password is not True", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+     }
 }
