ace27373c77118f1e22349de4ca1839837e091d3
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-07-17 21:53:50 +0430

    crate home items and add recyclerView home items and styles shape:

---
+++ app/src/main/java/com/example/myloginapp/HomeAdapter.java
@@ -0,0 +1,4 @@
+package com.example.myloginapp;
+
+public class HomeAdapter {
+}
--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -11,29 +11,29 @@ import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
-import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent intentSignUp = new Intent(this, HomeActivity.class);
+        Intent intentSignUp = new Intent(this, SignupActivity.class);
+        Intent intentHome = new Intent(this, HomeActivity.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView)  findViewById(R.id.password);
-        TextView signup_text = (TextView) findViewById(R.id.signup_now);
-        MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+        TextView signup_text = findViewById(R.id.signup_now);
+        MaterialButton login_btn = findViewById(R.id.login_btn);
 
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("mahdi") && password.getText().toString().equals("welcome") ){
-                    startActivity(intentSignUp);
+                    startActivity(intentHome);
                 }else{
                     Toast.makeText(MainActivity.this, "Username or password is not True", Toast.LENGTH_SHORT).show();
                 }
